c0c2e3e66f04809483384a4b532a93a17d509eb8
Jasmeet Singh <jascee.gtbit@gmail.com>
2019-09-10 21:05:45 +0530

    added endpoints for testing

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ var fs = require('fs');
 var app = express();
 var http = require('http');
 
+app.post('/login', function (req, res) {
+	res.status(200)
+	res.send(JSON.stringify({token: '321dqfadsf23dd'}))
+})
 
-app.get('/', function(req,res){ 
-	res.sendFile( __dirname + "/index.html");
+app.get('/logout', function (req,res) {
+	res.status(401);
+	res.send(JSON.stringify({statusMsg: 'Token Expired'}));
+})
+
+app.get('/getSomeData', function (req,res) {
+	const number = Math.floor(Math.random() * 20 + 1)
+	let data = []
+	for( var i = 0 ; i<  number; i++ ) {
+		data = [...data, {
+			name: `test${i}`,
+			id: i
+		}]
+	}
+	res.status(200);
+	res.send(JSON.stringify({data}));
+})
+
+app.get('/get500', function (req,res) {
+	res.status(500);
+	res.send(JSON.stringify({statusMsg: 'Something went wrong!'}));
 })
 
 var port = process.env.PORT || 3000;
